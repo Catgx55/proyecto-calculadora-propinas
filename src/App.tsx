684ba37fb9ab601f8a651460b1166ffed13d5e62
Cesar Trujillo <catgx55@gmail.com>
2024-05-16 13:38:41 -0500

    agregando componente OrderContents.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import MenuItem from "./components/MenuItem"
+import OrderContents from "./components/OrderContents"
 import { menuItems } from "./data/db"
 import useOrder from "./hooks/useOrder"
 
 function App() {
 
-  const {addItem} = useOrder()
+  const { order, addItem, removeItem} = useOrder()
 
   return (
     <>
@@ -16,7 +17,7 @@ function App() {
         <div className="p-5">
           <h2 className="text-4xl font-black">Menú</h2>
 
-          <div className="space-y-3">
+          <div className="mt-10 space-y-3">
             {menuItems.map(item => (
               <MenuItem
                 key={item.id}
@@ -27,8 +28,11 @@ function App() {
           </div>
         </div>
 
-        <div>
-          <h2>Consumo</h2>
+        <div className="border border-dashed border-slate-300 p-5 rounded-b-lg space-y-10">
+          <OrderContents
+            order={order}
+            removeItem={removeItem}
+          />
         </div>
       </main>
     </>
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -6,15 +6,26 @@ export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
 
     const addItem = (item: MenuItem) => {
+        //Evitando duplicados
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+        if(itemExist){
+            const updatedOrder = order.map(orderItem => orderItem.id === item.id ? {...orderItem, quantity: orderItem.quantity + 1} : orderItem)
+            setOrder(updatedOrder)
+        }else{
+            const newItem = {...item, quantity: 1}
+            setOrder([...order, newItem])
+        }
         
-        const newItem = {...item, quantity: 1}
-        setOrder([...order, newItem])
     }
 
-    console.log(order)
+    const removeItem = (id: MenuItem['id']) => {
+        setOrder(order.filter(item => item.id != id))
+    }
 
     return {
-        addItem
+        order,
+        addItem,
+        removeItem
 
     }
 }
