088db858364d8fe1ad44255ba38f3b2ff7b52d3d
Cesar Trujillo <catgx55@gmail.com>
2024-05-20 14:38:31 -0500

    agregando componente OrderTotal y TipPercentageForm.

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -4,6 +4,7 @@ import type { MenuItem, OrderItem } from "../types"
 
 export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
+    const [tip, setTip] = useState(0)
 
     const addItem = (item: MenuItem) => {
         //Evitando duplicados
@@ -22,10 +23,18 @@ export default function useOrder() {
         setOrder(order.filter(item => item.id != id))
     }
 
+    const placeOrder = () => {
+        setOrder([])
+        setTip(0)
+    }
+
     return {
         order,
+        tip,
+        setTip,
         addItem,
-        removeItem
+        removeItem,
+        placeOrder
 
     }
 }
